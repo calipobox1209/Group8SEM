d3b78f23f250d342efe4ad8d1f0785b8381cbbc8
Aycava <theavacya@gmail.com>
2024-03-12 00:24:12 +0000

    First edition of report factory class

---
+++ src/main/java/com/napier/sem/reportFactory.java
@@ -0,0 +1,35 @@
+package com.napier.sem;
+
+public class reportFactory {
+
+    //these functions will eventually have the return type of ArrayList(country/city)
+    public void countryReportMaker(int q, int n, String a)
+    {
+        int query = q;
+        switch (query) {
+
+            case 1:
+                System.out.println("test");
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+        }
+    }
+
+    public void cityReportMaker(int q, int n, String a)
+    {
+        int query = q;
+        switch (query) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+        }
+    }
+
+}
+

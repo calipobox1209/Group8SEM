9488735449783291775a47d54af8a2341c0671ec
connor12170@hotmail.co.uk <connor12170@hotmail.co.uk>
2024-03-12 20:41:05 +0000

    Created the blank classes for City and Country

---
+++ src/main/java/com/napier/sem/City.java
@@ -0,0 +1,26 @@
+package com.napier.sem;
+
+public class City {
+    String name;
+    String Country;
+    String District;
+    String Population;
+
+
+
+    public City reportAllCitiesByArea(String area){
+
+        return null;
+    }
+
+    public City reportNCitiesByArea(String area, int N){
+
+        return null;
+    }
+
+
+    public City singleCityPop(String area){
+
+        return null;
+    }
+}
---
+++ src/main/java/com/napier/sem/Country.java
@@ -0,0 +1,31 @@
+package com.napier.sem;
+
+public class Country {
+        String code;
+        String name;
+        String continent;
+        String region;
+        String population;
+        String capital;
+
+
+
+        public Country reportAllCountriesByArea(String area){
+
+            return null;
+        }
+
+        public Country reportNCitiesByArea(String area, int N){
+
+            return null;
+        }
+
+
+        public Country singleCountryPop(String area){
+
+            return null;
+        }
+
+
+    }
+

68d91fc9d33ae76f6b43f8799c7a3edf59bd4c26
connor12170@hotmail.co.uk <connor12170@hotmail.co.uk>
2024-03-13 00:49:16 +0000

    Created a basic outline for the methods responsible for populating the arrayLists. SQL Needed to make it run

--- src/main/java/com/napier/sem/City.java
+++ src/main/java/com/napier/sem/City.java
@@ -1,20 +1,67 @@
 package com.napier.sem;
 
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public class City {
     String name;
-    String Country;
-    String District;
-    String Population;
+    String country;
+    String district;
+    String population;
 
 
 
-    public City reportAllCitiesByArea(String area){
+    public ArrayList<City> reportAllCitiesByArea(String area){
+        try {
+            Statement stmt = con.createStatement();
+            String select = "";
+            ResultSet rset = stmt.executeQuery(select);
 
-        return null;
+            ArrayList<City> cities = new ArrayList<City>();
+            while (rset.next()) {
+                City city = new City();
+                city.name = rset.getString("city.Name");
+                city.district = rset.getString("city.District");
+                city.population = rset.getString("city.Population");
+                cities.add(city);
+            }
+            return cities;
+        }
+
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get city details");
+            return null;
+        }
+
+
     }
 
-    public City reportNCitiesByArea(String area, int N){
+    public ArrayList<City> reportNCitiesByArea(String area, int N){
+        try {
+            Statement Nstmt = con.createStatement();
+            String select = "";
+            ResultSet rset = Nstmt.executeQuery(select);
 
+            ArrayList<City> topNcities = new ArrayList<City>();
+            while (rset.next()) {
+                City Ncity = new City();
+                Ncity.name = rset.getString("city.Name");
+                Ncity.district = rset.getString("city.District");
+                Ncity.population = rset.getString("city.Population");
+                topNcities.add(Ncity);
+            }
+            return topNcities;
+        }
+
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get city details");
+            return null;
+        }
         return null;
     }
 

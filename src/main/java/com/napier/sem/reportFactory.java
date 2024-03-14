4ba43ee5d7d1f5065c243de2e9e7cf8da641b2ae
Aycava <theavacya@gmail.com>
2024-03-14 00:21:04 +0000

    Added new string to function input.

--- src/main/java/com/napier/sem/reportFactory.java
+++ src/main/java/com/napier/sem/reportFactory.java
@@ -7,33 +7,34 @@ public class reportFactory {
     //City cityReport = new City;
 
     //these functions will eventually have the return type of ArrayList(country/city)
-    public void countryReportMaker(int query, int n, String a)
+    public void countryReportMaker(int query, int n, String a, String b)
     {
         switch (query) {
 
             case 1:
-                //return countryReport.reportAllCountryByArea(a)
+
+                //return countryReport.reportAllCountryByArea(a,b)
                 break;
             case 2:
-                //return countryReport.reportNCountryByArea(n,a)
+                //return countryReport.reportNCountryByArea(n,a,b)
                 break;
             case 3:
-                //return countryReport.singleCountryPop(a)
+                //return countryReport.singleCountryPop(b)
                 break;
         }
     }
 
-    public void cityReportMaker(int query, int n, String a)
+    public void cityReportMaker(int query, int n, String a, String b)
     {
         switch (query) {
             case 1:
-                //return cityReport.reportAllCitiesByArea(a)
+                //return cityReport.reportAllCitiesByArea(a,b)
                 break;
             case 2:
-                //return cityReport.reportNCitiesByArea(a,n)
+                //return cityReport.reportNCitiesByArea(n,a,b)
                 break;
             case 3:
-                //return cityReport.singleCityPop(a)
+                //return cityReport.singleCityPop(b)
                 break;
         }
     }

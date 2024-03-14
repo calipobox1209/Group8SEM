ef380cdcabc2713b310df67a14a83ec531af9205
Aycava <159350145+Aycava@users.noreply.github.com>
2024-03-14 10:38:27 +0000

    Update Country.java
    Added comments to clarify code

--- src/main/java/com/napier/sem/Country.java
+++ src/main/java/com/napier/sem/Country.java
@@ -1,5 +1,6 @@
 package com.napier.sem;
-
+//this class has similar functionality to the city class with the main obvious difference being that it handles country related report queries
+//as with the last class a lot of things will be repeated within these functions, with the main differences being present in the sql
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -11,17 +12,19 @@ public class Country {
     String region;
     String population;
     String capital;
+    //instantiates singleton instance to facility connectivity to the database
     ConnectionProvider a = ConnectionProvider.getInstance();
-
+       //report generation function for all countries by user specified area
        public ArrayList<Country> reportAllCountriesByArea(String area, String areaName){
                 try {
                     Statement stmt = a.con.createStatement();
+                    //sql statement that injects user input from main in order to specify results
                     String select = "SELECT Code, Name, Continent, Region, Population, Capital " +
                             "FROM country " +
                             "WHERE " + area + " = " + "'" + areaName + "' " +
                             "ORDER BY Population DESC ";
                     ResultSet rset = stmt.executeQuery(select);
-
+                    //creates country objects and populates countries arraylist
                     ArrayList<Country> countries = new ArrayList<Country>();
                     while (rset.next()) {
                         Country country = new Country();
@@ -32,7 +35,7 @@ public class Country {
                         country.population = rset.getString("country.Population");
                         country.capital = rset.getString("country.capital");
                         countries.add(country);
-                    }
+                    }//returns arraylist to factory class
                     return countries;
                 }
 
@@ -43,15 +46,16 @@ public class Country {
                     return null;
                 }
         }
-
+    //function reponsible for the all countries by world special case
     public ArrayList<Country> reportAllCountriesByWorld(){
         try {
             Statement stmt = a.con.createStatement();
+            //trimmed down version of all countries by area statement
             String select = "SELECT Code, Name, Continent, Region, Population, Capital " +
                     "FROM country " +
                     "ORDER BY Population DESC ";
             ResultSet rset = stmt.executeQuery(select);
-
+            //creates countries, populates arraylist
             ArrayList<Country> countries = new ArrayList<Country>();
             while (rset.next()) {
                 Country country = new Country();
@@ -74,18 +78,19 @@ public class Country {
         }
     }
 
-
+        //function responsible for generating reports of user specified length and user specified area
         public ArrayList<Country> reportNCountriesByArea(String area, String areaName, int N){
 
             try {
                 Statement stmt = a.con.createStatement();
+                //sql with user input injection
                 String select = "SELECT Code, Name, Continent, Region, Population, Capital " +
                         "FROM country " +
                         "WHERE " + area + " = '" + areaName + "' " +
                         "ORDER BY Population DESC LIMIT " + N;
 
                 ResultSet rset = stmt.executeQuery(select);
-
+                //make countries, fill array
                 ArrayList<Country> countries = new ArrayList<Country>();
                 while (rset.next()) {
                     Country country = new Country();
@@ -105,17 +110,18 @@ public class Country {
                 return null;
             }
         }
-
+    //another special case for the world area option in the user specified length type
     public ArrayList<Country> reportNCountriesByWorld(int N){
 
         try {
             Statement stmt = a.con.createStatement();
+            //trimmed down version of report n countries by area statment
             String select = "SELECT Code, Name, Continent, Region, Population, Capital " +
                     "FROM country " +
                     "ORDER BY Population DESC LIMIT " + N;
 
             ResultSet rset = stmt.executeQuery(select);
-
+            //countries,array
             ArrayList<Country> countries = new ArrayList<Country>();
             while (rset.next()) {
                 Country country = new Country();
@@ -135,7 +141,7 @@ public class Country {
             return null;
         }
     }
-
+    //special case for reporting only a single country
     public ArrayList<Country> reportSingleCountry(String countryName){
         try {
             Statement stmt = a.con.createStatement();
@@ -145,7 +151,7 @@ public class Country {
                     "WHERE country.Name = '" + countryName + "'";
 
             ResultSet rset = stmt.executeQuery(select);
-
+            //c,a
             ArrayList<Country> countries = new ArrayList<Country>();
             while (rset.next()) {
                 Country country = new Country();
